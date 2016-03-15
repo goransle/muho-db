fa5540b5290df12d458e566441e5bde3bd575cc3
goransle <goransle@gmail.com>
2016-03-15 14:53:09 +0100

    små endringer

--- models/Post.js
+++ models/Post.js
@@ -14,10 +14,8 @@ var Post = new keystone.List('Post', {
 Post.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	//author: { type: Types.Relationship, ref: 'User', index: true },
-	//publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	sources: { type: Types.Url, many:true },
-	year: { type: Types.Number, index: true },
+	author: { type: Types.Relationship, ref: 'User', index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.LocalFile,
         dest: './public/images',
         prefix: '/images',
@@ -26,9 +24,11 @@ Post.add({
         }},
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+		extended: { type: Types.Html, wysiwyg: true, height: 400 },
+		sources: { type: Types.Url, many:true },
+		eventYear: { type: Types.Number, index: true }
 	},
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
+	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
 });
 
 Post.schema.virtual('content.full').get(function() {
