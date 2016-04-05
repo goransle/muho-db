95f9eb7f9d08c2157dd32927544a55f8dd11eeda
larseholte <larseholte@outlook.com>
2016-04-05 10:25:07 +0200

    Kateforiendring + statisk innhold ("sjekkliste")
    Indexen har fått noe statisk innhold som bare skal være som en
    sjekkliste for ting vi må fikse. PostCategory har fått description som
    kan trekkes til index for oppsumeringstekst

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -6,11 +6,12 @@ var keystone = require('keystone');
  */
 
 var PostCategory = new keystone.List('PostCategory', {
-	autokey: { from: 'name', path: 'key', unique: true }
+	autokey: { from: 'name', path: 'key', unique: true },
 });
 
 PostCategory.add({
-	name: { type: String, required: true }
+	name: { type: String, required: true },
+	description: {type: String, required: false }
 });
 
 PostCategory.relationship({ ref: 'Post', path: 'categories' });
